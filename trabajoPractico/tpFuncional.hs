f382d0f9061483424839b27dbb0b1283941ba5dd
Bruno <ruiz.brunoivan@gmail.com>
2019-04-13 12:41:44 -0300

    Add carpeta TP Funcional

---
+++ trabajoPractico/tpFuncional.hs
@@ -0,0 +1,15 @@
+--Punto 1.
+data Planta = Planta { nombrePlanta::String, puntosDeVida::Int, generaSoles::Int, poderDeAtaque::Int} deriving (Show)
+data Zombie = Zombie { nombreZombie::String, nivelDeMuerte::Int, articulos::Int, poderDeMordida::Int} deriving (Show)
+
+peaShooter = Planta {nombrePlanta="Peashooter", puntosDeVida=5, generaSoles=0, poderDeAtaque=2}
+repeater = Planta { nombrePlanta="Repeater", puntosDeVida=5, generaSoles=0, poderDeAtaque=4}
+sunFlower = Planta { nombrePlanta="Sun Flower", puntosDeVida=7, generaSoles=1, poderDeAtaque=0}
+nut = Planta { nombrePlanta="Nut", puntosDeVida=100, generaSoles=0, poderDeAtaque=0}
+zombieBase = Zombie { nombreZombie="Zombie", nivelDeMuerte=6, articulos=0, poderDeMordida=1}
+ballonZombie = Zombie { nombreZombie="Pepe colgado", nivelDeMuerte=12, articulos=1, poderDeMordida=1}
+newspaperZombie = Zombie { nombreZombie="Beto el chismoso", nivelDeMuerte=16, articulos=1, poderDeMordida=2}
+gargantuar = Zombie { nombreZombie="Gargantuar Hulk Smash Puny God", nivelDeMuerte=30, articulos=2, poderDeMordida=30}
+
+--Punto 2.
+especialidad puntosDeVida poderDeAtaque generaSoles |(poderDeAtaque*2)>puntosDeVida = "Atacante" |generaSoles==1 = "Proovedora" |otherwise "Defensiva"
