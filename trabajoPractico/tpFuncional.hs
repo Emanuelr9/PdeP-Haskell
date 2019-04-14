c0b2b31135749e5813ca40f27176f1ed527c58a9
Ignacio Ferrentino <iferren@gmail.com>
2019-04-14 13:40:03 -0300

    Commit con lo hecho el 13-4-19 en la facu

--- trabajoPractico/tpFuncional.hs
+++ trabajoPractico/tpFuncional.hs
@@ -1,4 +1,4 @@
---Punto 1.
+{-- --Punto 1.
 data Planta = Planta { nombrePlanta::String, puntosDeVida::Int, generaSoles::Int, poderDeAtaque::Int} deriving (Show)
 data Zombie = Zombie { nombreZombie::String, nivelDeMuerte::Int, articulos::Int, poderDeMordida::Int} deriving (Show)
 
@@ -13,3 +13,29 @@ gargantuar = Zombie { nombreZombie="Gargantuar Hulk Smash Puny God", nivelDeMuer
 
 --Punto 2.
 especialidad puntosDeVida poderDeAtaque generaSoles |(poderDeAtaque*2)>puntosDeVida = "Atacante" |generaSoles==1 = "Proovedora" |otherwise "Defensiva"
+--}
+
+--Punto 1 --
+
+data Planta = Planta {
+    nombre::String,puntosDeVida :: Int,
+    generaSoles :: Int,
+    poderDeAtaque :: Int
+}deriving (Show)
+
+data Zombie = Zombie {
+    nombreZombie :: String,
+    nivelDeMuerte :: Int,
+    articulos :: Int,
+    poderDeMordida :: Int
+}deriving (Show)
+
+peaShooter = Planta {nombre = "Peashooter" , puntosDeVida = 5, generaSoles =0, poderDeAtaque = 2}
+repeater = Planta {nombre = "Repeater" , puntosDeVida = 5, generaSoles =0, poderDeAtaque = 4} -- hacer con copia?
+sunFlower = Planta {nombre = "Sunflower" , puntosDeVida = 7, generaSoles =1, poderDeAtaque = 0}
+nut = Planta {nombre = "Nut" , puntosDeVida = 100, generaSoles =0, poderDeAtaque = 0}
+
+zombieBase = Zombie {nombreZombie = "Zombie", nivelDeMuerte = 6, articulos=0, poderDeMordida=1}
+ballonZombie = Zombie {nombreZombie = "Pepe colgado", nivelDeMuerte = 12, articulos=1, poderDeMordida=1}
+paperZombie = Zombie {nombreZombie = "Betl el chismoso", nivelDeMuerte = 16, articulos=1, poderDeMordida=2}
+gargantuar = Zombie {nombreZombie = "Gargantuar Hulk Smash Puni god", nivelDeMuerte = 30, articulos=2, poderDeMordida=30}
